2d3762fc943195f94bd535b63fb0f28d70fdfefa
Your Name <you@example.com>
2023-11-17 16:48:35 +0500

    madina

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const home = document.querySelector(`#home-link`)
+home.addEventListener(`click`, (e) => {
+    window.location.href = `../index.html`
+})
+
+const link = document.querySelector(`#link-register`)
+link.addEventListener(`click`, (e) => {
+    window.location.href = `../register.html`
+})
+
+const blogLink = document.querySelector(`#blog-link`)
+blogLink.addEventListener(`click` , (e) =>{
+    window.location.href =`../all.html`;
+    console.log(`salom`);
+})
+
+const  Bussines = document.querySelector(`#Bussines-link`)
+home.addEventListener(`click`, (e) => {
+    window.location.href = `../index.html`
+})
+
+Bussines.addEventListener(`click`, (e) => {
+    window.location.href = "../busness.html"
+})
